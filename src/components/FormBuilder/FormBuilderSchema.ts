87ac37947a800f360e3f7427ef960f03a5fd76f1
jtyler <jtyler.dev@gmail.com>
2025-02-10 23:13:09 -0800

    updating builder schema

--- src/components/FormBuilder/FormBuilderSchema.ts
+++ src/components/FormBuilder/FormBuilderSchema.ts
@@ -1,23 +1,49 @@
 import z from "zod";
 import { FormFieldType } from "@prisma/client";
 
-export const FormFieldSchema = z.object({
-  id: z.string(),
-  label: z.string().min(1, "Label is required"),
-  type: z.enum([
-    FormFieldType.TEXT,
-    FormFieldType.TEXTAREA,
-    FormFieldType.SELECT,
-    FormFieldType.CHECKBOX,
-    FormFieldType.RADIO,
-    FormFieldType.DATE,
-    FormFieldType.MULTI_SELECT,
-    FormFieldType.NUMBER,
-    FormFieldType.FILE,
-  ]),
-  options: z.array(z.string()).optional(),
-  isRequired: z.boolean().optional(),
-});
+export const FormFieldSchema = z
+  .object({
+    id: z.string(),
+    question: z.string().min(1, "Question is required"),
+    description: z.string().optional(),
+    type: z.enum([
+      FormFieldType.TEXT,
+      FormFieldType.TEXTAREA,
+      FormFieldType.SELECT,
+      FormFieldType.CHECKBOX,
+      FormFieldType.RADIO,
+      FormFieldType.DATE,
+      FormFieldType.TIME,
+      FormFieldType.MULTI_SELECT,
+      FormFieldType.NUMBER,
+      FormFieldType.FILE,
+    ]),
+    options: z.array(z.string()).optional(),
+    isRequired: z.boolean().optional(),
+  })
+  .refine(
+    (data) => {
+      // refine the definition to ensure that options are required for certain field types
+      // even though it is optional in the schema. This is because the options are required
+      // for the field to be valid
+      if (
+        data.type === FormFieldType.SELECT ||
+        data.type === FormFieldType.CHECKBOX ||
+        data.type === FormFieldType.RADIO ||
+        data.type === FormFieldType.MULTI_SELECT
+      ) {
+        return (
+          data.options && Array.isArray(data.options) && data.options.length > 0
+        );
+      }
+
+      return true;
+    },
+    {
+      message: "Options are required for this field type",
+      path: ["options"],
+    }
+  );
 
 export const FormBuilderSchema = z.object({
   title: z.string().min(1, "Title is required"),
